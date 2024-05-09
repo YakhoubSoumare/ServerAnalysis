a3ddf08e0466e4753e3d70e89e8e4f66a6974a7f
YakhoubSoumare <76662360+YakhoubSoumare@users.noreply.github.com>
2024-05-09 22:33:45 +0200

    need of fixing the dropdown of hamburger menu

--- serveranalysisfrontend/src/App.js
+++ serveranalysisfrontend/src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Serververless Functions vs. Server-based Applications</h1>
+        <nav className="navbar">
+          <div className="hamburger-menu" onClick={() => setIsOpen(!isOpen)}>☰</div>
+          <ul>
+            <li>Home</li>
+            <li>Thesis</li>
+          </ul>
+          <div className={isOpen ? 'dropdown open' : ''}>
+            <ul>
+              <li>Home</li>
+              <li>Thesis</li>
+            </ul>
+          </div>
+        </nav>
       </header>
     </div>
   );
