8910d6f11e21a5f7424be937debb2d2ac4f3161b
YakhoubSoumare <76662360+YakhoubSoumare@users.noreply.github.com>
2024-05-10 15:33:56 +0200

    implemented responsive header, navbar and hamburger menu

--- serveranalysisfrontend/src/App.js
+++ serveranalysisfrontend/src/App.js
@@ -1,25 +1,29 @@
+import React , { useState } from 'react';
 import './App.css';
-import { useState } from 'react';
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Serververless Functions vs. Server-based Applications</h1>
-        <nav className="navbar">
-          <div className="hamburger-menu" onClick={() => setIsOpen(!isOpen)}>☰</div>
-          <div className={isOpen ? 'menu' : 'menu open'}>
-            <ul>
-              <li>Home</li>
-              <li>Thesis</li>
-            </ul>
-          </div>
-        </nav>
-      </header>
-    </div>
-  );
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <div className="App">
+            <header className="app-header">
+                <div className="non-mobile-header">
+                    <h1>Serververless Functions vs. Server-based Applications</h1>
+                </div>
+                <nav className="navbar">
+                    <ul className={isOpen ? '' : 'dropdown'}>
+                        <li className="home">Home</li>
+                        <li>About</li>
+                        <li>Thesis</li>
+                    </ul>
+                    <ul>  
+                      <li className="hamburger" onClick={() => {setIsOpen(!isOpen)}}>☰</li>
+                    </ul>
+                </nav>
+                <h1 className="mobile-header">Serververless Functions vs. Server-based Applications</h1>
+            </header>
+        </div>
+    );
 }
-
 export default App;
