b9796d913d7f42fb0a8f7fe875d30b4335599e92
YakhoubSoumare <76662360+YakhoubSoumare@users.noreply.github.com>
2024-05-11 01:27:10 +0200

    added routes and useffects

--- serveranalysisfrontend/src/App.js
+++ serveranalysisfrontend/src/App.js
@@ -1,29 +1,67 @@
-import React , { useState } from 'react';
+import React , { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
 import './App.css';
+import Home from './components/Home';
+import Thesis from './components/Thesis';
+import About from './components/About';
 
 function App() {
 
     const [isOpen, setIsOpen] = useState(false);
 
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth > 600) {
+                setIsOpen(false);
+            }
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
-        <div className="App">
-            <header className="app-header">
-                <div className="non-mobile-header">
-                    <h1>Serververless Functions vs. Server-based Applications</h1>
-                </div>
-                <nav className="navbar">
-                    <ul className={isOpen ? '' : 'dropdown'}>
-                        <li className="home">Home</li>
-                        <li>Thesis</li>
-                        <li>About</li>
-                    </ul>
-                    <ul>  
-                      <li className="hamburger" onClick={() => {setIsOpen(!isOpen)}}>☰</li>
-                    </ul>
-                </nav>
-                <h1 className="mobile-header">Serververless Functions vs. Server-based Applications</h1>
-            </header>
-        </div>
+        <Router>
+            <div className="App">
+                <header className="app-header">
+                    <div className="non-mobile-header">
+                        <h1>Serververless Functions vs. Server-based Applications</h1>
+                    </div>
+                    <nav className="navbar">
+                        <ul className={isOpen ? 'dropdown' : ''}>
+                            <li className="home">
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                            <Link to="/thesis">Thesis</Link>
+                            </li>
+                            <li>
+                            <Link to="/about">About</Link>
+                            </li>
+                        </ul>
+                        <ul>  
+                        <li className="hamburger" onClick={() => {setIsOpen(!isOpen)}}>☰</li>
+                        </ul>
+                    </nav>
+                </header>
+                <body className="app-body">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/thesis" element={<Thesis />} />
+                        <Route path="/about" element={<About />} />
+                    </Routes>
+                </body>
+                <footer className="app-footer">
+                    <p>© 2024 Serverless Functions vs. Server-based Applications</p>
+                    <p>Thesis by Yakhoub Soumare</p>
+                    <p>Presented to IT-Högskolan</p>
+                    <p>In collaboration with Meta Bytes</p>
+                </footer>
+            </div>
+        </Router>
     );
 }
 export default App;
