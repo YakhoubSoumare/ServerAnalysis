55b46634682fcd71a1afc8cdaf29a6d94e1c6cfa
YakhoubSoumare <76662360+YakhoubSoumare@users.noreply.github.com>
2024-05-16 14:58:43 +0200

    Added image model, controller, dto, profile and configurations needed

--- ServerAnalysisAPI/DTOs/TopicDto.cs
+++ ServerAnalysisAPI/DTOs/TopicDto.cs
@@ -4,12 +4,12 @@ public class TopicDto
 {
 	public int Id { get; set; }
 	public string Title { get; set; } = string.Empty;
-	public string Introduction { get; set; } = string.Empty;
-	public string Approach { get; set; } = string.Empty;
+	public string Introductions { get; set; } = string.Empty;
+	public string Approaches { get; set; } = string.Empty;
 	public string UseCases { get; set; } = string.Empty;
 	public string Limitations { get; set; } = string.Empty;
 	public string Advantages { get; set; } = string.Empty;
-	public string Comparison { get; set; } = string.Empty;
+	public string Comparisons { get; set; } = string.Empty;
 	public string IndustryInsights { get; set; } = string.Empty;
 	public string Beneficiaries { get; set; } = string.Empty;
 }
---
+++ ServerAnalysisAPI/Migrations/20240516113748_columnsAllPlural.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServerAnalysisAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class columnsAllPlural : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Introduction",
+                table: "Topics",
+                newName: "Introductions");
+
+            migrationBuilder.RenameColumn(
+                name: "Comparison",
+                table: "Topics",
+                newName: "Comparisons");
+
+            migrationBuilder.RenameColumn(
+                name: "Approach",
+                table: "Topics",
+                newName: "Approaches");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Introductions",
+                table: "Topics",
+                newName: "Introduction");
+
+            migrationBuilder.RenameColumn(
+                name: "Comparisons",
+                table: "Topics",
+                newName: "Comparison");
+
+            migrationBuilder.RenameColumn(
+                name: "Approaches",
+                table: "Topics",
+                newName: "Approach");
+        }
+    }
+}
