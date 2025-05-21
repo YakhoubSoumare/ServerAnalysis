984a37b28d42484af00f84fd4041b44c9fa8a116
YakhoubSoumare <yakhoub.soumare1990@gmail.com>
2025-05-22 00:43:47 +0200

    Add initial Terraform configuration for Azure infrastructure: - Defined resources for App Service, SQL Server, and Resource Group - Included variables, outputs, and backend setup - Marked sensitive outputs for secure handling

---
+++ terraform/.terraform.lock.hcl
@@ -0,0 +1,41 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/azurerm" {
+  version     = "3.80.0"
+  constraints = "~> 3.80.0"
+  hashes = [
+    "h1:hmO/U2cTWnKuRZBLQj68ZsdiknsAA838L3Oe/lTjpAc=",
+    "zh:11e3362a3d9ca9d6d57a3c863b26ddcb9e4366c6c99586a4149d2842976e998c",
+    "zh:3005446ee10379e5354b2388d788cad92068ff0988f50e49f6e8eeacdbd6e0a1",
+    "zh:45aa9f9d930e332fc68f2093232edf15ef0d087c9588317371838784382b9dbc",
+    "zh:4e1d57b6b909577b37e85117533eb0d6a2128e7ccfcf88e709ad890f86e67295",
+    "zh:5f777dbb7d72ab83a3a9b9412f8e341573fb34591c834fefa8fc7593e73232a2",
+    "zh:6fab9ea69a2177e29a0c0819f32c51245891fd19b807c4a3e2d4dd5c0a28b8fb",
+    "zh:77b1bdf671a83e17c82b30520312970109811c7725b61fcef69138244d7dd212",
+    "zh:8a1e8d9243f1149b7bcf7b8bda3cf5a1bcea6c709b55053fe7e8cd767dd5f632",
+    "zh:a7c1de65d5f1f241c4d3f30f24912cb9dc8315a1dc7d7bd0814cb4beb1e8f1fb",
+    "zh:b4385e7c84d708cd81647555dd12ade9fb128fd027eb59cd651f4cdcf98a437e",
+    "zh:d4ef3f5b126d5c9766300ffb6f1b3ed091484d46c742cfae2645261e749162e5",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/random" {
+  version = "3.7.2"
+  hashes = [
+    "h1:0hcNr59VEJbhZYwuDE/ysmyTS0evkfcLarlni+zATPM=",
+    "zh:14829603a32e4bc4d05062f059e545a91e27ff033756b48afbae6b3c835f508f",
+    "zh:1527fb07d9fea400d70e9e6eb4a2b918d5060d604749b6f1c361518e7da546dc",
+    "zh:1e86bcd7ebec85ba336b423ba1db046aeaa3c0e5f921039b3f1a6fc2f978feab",
+    "zh:24536dec8bde66753f4b4030b8f3ef43c196d69cccbea1c382d01b222478c7a3",
+    "zh:29f1786486759fad9b0ce4fdfbbfece9343ad47cd50119045075e05afe49d212",
+    "zh:4d701e978c2dd8604ba1ce962b047607701e65c078cb22e97171513e9e57491f",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:7b8434212eef0f8c83f5a90c6d76feaf850f6502b61b53c329e85b3b281cba34",
+    "zh:ac8a23c212258b7976e1621275e3af7099e7e4a3d4478cf8d5d2a27f3bc3e967",
+    "zh:b516ca74431f3df4c6cf90ddcdb4042c626e026317a33c53f0b445a3d93b720d",
+    "zh:dc76e4326aec2490c1600d6871a95e78f9050f9ce427c71707ea412a2f2f1a62",
+    "zh:eac7b63e86c749c7d48f527671c7aee5b4e26c10be6ad7232d6860167f99dbb0",
+  ]
+}
---
+++ terraform/main.tf
@@ -0,0 +1,108 @@
+# Generates a unique resource group name using a random pet name
+resource "random_pet" "rg_name" {
+  prefix      = var.resource_group_name_prefix
+}
+
+# Creates an Azure resource group with a unique name and specified location
+resource "azurerm_resource_group" "rg" {
+  name        = random_pet.rg_name.id
+  location    = var.resource_group_location
+}
+
+# Generates a unique SQL Server name using a random pet name with "sql" prefix
+resource "random_pet" "azurerm_mssql_server_name" {
+  prefix      = "sql"
+}
+
+# Generates a random password with specific criteria (length, special characters, and complexity).
+resource "random_password" "admin_password" {
+  count         = var.admin_password == null ? 1 : 0
+  length        = var.password_length >= 10 ? var.password_length : 10     # Make sure it's at least 10
+  special       = true
+  min_numeric   = 1
+  min_upper     = 1
+  min_lower     = 1
+  min_special   = 1
+}
+
+# Attempts to use the randomly generated admin password; falls back to a manually provided password if generation fails.
+locals {
+  admin_password    = try(random_password.admin_password[0].result, var.admin_password)
+}
+
+# SQL Server creation in the new resource group
+resource "azurerm_mssql_server" "server" {
+  name                            = random_pet.azurerm_mssql_server_name.id
+  resource_group_name             = azurerm_resource_group.rg.name         # Reference the new resource group
+  location                        = azurerm_resource_group.rg.location     # Same location as before
+  administrator_login             = var.admin_username
+  administrator_login_password    = local.admin_password
+  version                         = "12.0"
+  minimum_tls_version             = "1.2"                                      # Enforcing TLS 1.2 for secure connections
+
+  # Sets connectivity method to Public Endpoint
+  public_network_access_enabled   = true                                       # Enabling public endpoint
+
+  # Sets Connection policy to Default
+  connection_policy               = "Default"
+}
+
+# Allowing list of IPs from tfvars
+resource "azurerm_mssql_firewall_rule" "allow_appservice_outbound" {
+  for_each = toset(var.appservice_outbound_ips)
+
+  name              = "AllowAppIP-${replace(each.value, ".", "-")}"  # Name needs safe chars
+  server_id         = azurerm_mssql_server.server.id
+  start_ip_address  = each.value                                     # IP stays unchanged
+  end_ip_address    = each.value
+}
+
+resource "azurerm_mssql_database" "db"{
+  name                             = var.sql_db_name
+  server_id                        = azurerm_mssql_server.server.id
+  collation                        = "SQL_Latin1_General_CP1_CI_AS"
+  max_size_gb                      = 32
+  sku_name                         = "GP_S_Gen5_1"
+  auto_pause_delay_in_minutes      = 60
+  min_capacity                     = 0.5
+  zone_redundant                   = false
+  read_scale                       = false
+  # license_type                     = "LicenseIncluded"                 # Not valid here (serverless). Only valid for provisioned vCore. 
+  elastic_pool_id                  = null                                # Ensures it's not part of an elastic pool
+  storage_account_type             = "Local"                             # Ensures that the storage_account_type is not geo (cost reasons)
+
+  tags = {}
+}
+
+resource "azurerm_service_plan" "asp" {
+  name                = "asp-${var.environment}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku_name = var.app_service_plan_sku_size
+
+  os_type             = "Windows"  # Defines it as Windows-based
+}
+
+resource "azurerm_windows_web_app" "web_app" {
+  name                = "server-analysis-api-${var.environment}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id = azurerm_service_plan.asp.id
+
+  site_config {
+    always_on = false  # Explicitly disable "Always On"
+  }
+
+  app_settings = {
+    "WEBSITE_RUN_FROM_PACKAGE" = "1"
+  }
+
+  connection_string {
+    name  = "AzureConnection"
+    type  = "SQLAzure"
+    value = var.connection_string_value
+  }
+
+  https_only = true
+}
